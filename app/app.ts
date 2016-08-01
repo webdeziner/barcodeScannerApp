1cb36423034f82d3ef75f2864efc3f83a2d9f837
Victor Eliasson <victor.eliasson@lenson.com>
2016-08-01 15:39:11 +0200

    Working example

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
-import {StatusBar} from 'ionic-native';
+import {StatusBar, BarcodeScanner} from 'ionic-native';
 import {HomePage} from './pages/home/home';
 
 
@@ -14,6 +14,7 @@ export class MyApp {
         this.rootPage = HomePage;
 
         platform.ready().then(() => {
+            console.log("App ready");
             // Okay, so the platform is ready and our plugins are available.
             // Here you can do any higher level native things you might need.
             StatusBar.styleDefault();
